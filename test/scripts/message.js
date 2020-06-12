1a7186d7c95b199364ccc874023d3890975c77d1
AH-REM <35509872+AH-REM@users.noreply.github.com>
2020-06-12 22:09:03 +0200

    Add message script

---
+++ test/scripts/message.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const Discord = require('discord.js');
+
+const options = {
+    message: {
+        guild: null,
+        channel: null,
+        content: new Array(),
+        react: new Array()
+    },
+    aliases: new Array(),
+    delete: -1
+};
+
+module.exports = (command, message) => {
+
+    command.options = Discord.Util.mergeDefault(options, command.options);
+    
+    const response = getMessage(command);
+    if (response) {
+
+        const channel = getChannel(command, message);
+        channel.send(response)
+            .then(m => {
+                addReact(command, m);
+            })
+            .catch(console.error);
+
+    }
+
+    if (command.options.delete > -1)
+        message.delete({timeout: command.options.delete});
+};
+
+function getMessage(command) {
+    const rand = Math.floor(Math.random() * command.options.message.content.length);
+    return command.options.message.content[rand];
+}
+
+function getChannel(command, message) {
+    const { client } = message;
+    const { guild, channel } = command.options.message;
+
+    if (guild && channel) {
+        const chan = client.guilds.cache.get(guild).channels.cache.get(channel);
+        return chan;
+    }
+    else return message.channel;
+}
+
+function addReact(command, message) {
+    const { react } = command.options.message;
+    if (react.length < 1) return;
+    for (let emoji of react) {
+        // Gérer les erreurs
+        message.react(emoji).catch(console.error);
+    }
+}
