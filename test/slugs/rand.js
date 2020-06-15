a1d4d273612279b07d46c9d58d5a315d4b52d4dd
AH-REM <35509872+AH-REM@users.noreply.github.com>
2020-06-16 01:00:35 +0200

    Update slug rand

--- test/slugs/rand.js
+++ test/slugs/rand.js
@@ -6,9 +6,9 @@ module.exports = (eventEmitter, arg) => {
 
     if (!arg) arg = 1;
 
-    const nb = parseInt(arg + 1, 10);
+    const nb = parseInt(arg, 10);
     if (!isNaN(nb)) {
-        return Math.floor(Math.random() * nb);
+        return Math.floor(Math.random() * (nb + 1));
     }
 
 }
