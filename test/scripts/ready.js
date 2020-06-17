aaab27d2743ebd5fe28200dfcd231a94cd44eb35
AH-REM <35509872+AH-REM@users.noreply.github.com>
2020-06-17 03:00:47 +0200

    Add message parser to ready script

--- test/scripts/ready.js
+++ test/scripts/ready.js
@@ -1,9 +1,19 @@
 'use strict';
 
+// const Noob = require('discord-noob');
+const Noob = require('../../src/');
+
 module.exports = (client, options) => {
 
     if (options.console) {
-        console.log(options.console);
+
+        let message = options.console;
+
+        if (options.parse) {
+            message = Noob.parserMessage(client, client, message);
+        }
+
+        console.log(message);
     }
 
 };
