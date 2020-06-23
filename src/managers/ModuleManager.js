67f83929b6970e55890541fb77f707380437bf54
AH-REM <35509872+AH-REM@users.noreply.github.com>
2020-06-24 00:23:10 +0200

    Update ModuleManager

--- src/managers/ModuleManager.js
+++ src/managers/ModuleManager.js
@@ -15,10 +15,10 @@ class ModuleManager {
 
         if (client.noobOptions[type] && fs.existsSync(Util.getCurrentPath(client.noobOptions[type] + name + ".js"))) {
             moduleLoaded = require(Util.getCurrentPath(client.noobOptions[type] + name));
-        } else if (fs.existsSync('../modules/' + type + '/' + name + ".js")) {
+        } else if (fs.existsSync(__dirname + '/../modules/' + type + '/' + name + ".js")) {
             moduleLoaded = require('../modules/' + type + '/' + name);
         }
-
+        
         return moduleLoaded;
     }
 
